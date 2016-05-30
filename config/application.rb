14b16113e3fd7e8dab92b4745c70b34b61bdcc2c
Guto Marzagao <gutomarzagao@gmail.com>
2016-05-30 00:36:59 -0300

    Enable assets digest

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module AirSoccer
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.digest = true
   end
 end
