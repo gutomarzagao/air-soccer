194fcb305396c9634088f4c8804ff10a2938ca44
Guto Marzagao <gutomarzagao@gmail.com>
2016-05-30 01:41:46 -0300

    Move player through keyboard

--- app/assets/javascripts/Player.js
+++ app/assets/javascripts/Player.js
@@ -11,4 +11,12 @@ function Player(posX, posY) {
 	this.circle.lineStyle(PLAYER_BORDER, PLAYER_BORDER_COLOR, 1);
 	this.circle.drawCircle(posX, posY, PLAYER_SIZE / 2);
 	this.circle.endFill();
+
+	this.velX = 0;
+	this.velY = 0;
 }
+
+Player.prototype.update = function() {
+	this.circle.x += this.velX;
+    this.circle.y += this.velY;
+};
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -11,10 +11,40 @@ $(function() {
 
 	var stage = new PIXI.Container();
 
-	renderer.render(stage);
-
 	var player = new Player(100, 100);
 	stage.addChild(player.circle);
 
-	renderer.render(stage);
+	$(document).keydown(function(evt) {
+		evt.preventDefault();
+
+		if (evt.which == 37) {
+			player.velX = -2;
+		} else if (evt.which == 38) {
+			player.velY = -2;
+		} else if (evt.which == 39) {
+			player.velX = 2;
+		} else if (evt.which == 40) {
+			player.velY = 2;
+		}
+	});
+
+	$(document).keyup(function(evt) {
+		evt.preventDefault();
+
+		if (evt.which == 37 || evt.which == 39) {
+			player.velX = 0;
+		} else if (evt.which == 38 || evt.which == 40) {
+			player.velY = 0;
+		}
+	});
+
+	gameLoop();
+
+	function gameLoop() {
+		requestAnimationFrame(gameLoop);
+
+		player.update();
+
+		renderer.render(stage);
+	}
 });
